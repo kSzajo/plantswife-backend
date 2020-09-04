62d1120bc458d35d912022dc2adb4ce2edc24a9c
krzysztofszajowski <krzysztof.szajowski@gmail.com>
2020-09-04 17:02:26 +0200

    Implement plant process watering/feeding/spraing

--- src/plant/service/plantProcess.service.ts
+++ src/plant/service/plantProcess.service.ts
@@ -1,50 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Plant } from '../entity/plant.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Watering } from '../entity/watering.entity';
 import { Feeding } from '../entity/feeding.entity';
 import { Spraing } from '../entity/spraing.entity';
+import { ProcessInterval } from '../dto/processInterval.enum';
+import * as moment from 'moment';
+import { unitOfTime } from 'moment';
 
 @Injectable()
 export class PlantProcessService {
 
   constructor(@InjectRepository(Watering) private wateringRepository: Repository<Watering>,
               @InjectRepository(Spraing) private spraingRepository: Repository<Spraing>,
+              @InjectRepository(Plant) private plantRepository: Repository<Plant>,
               @InjectRepository(Feeding) private feedingRepository: Repository<Feeding>) {
   }
 
-  watering(id: number): Promise<Watering> {
-    const plant = new Plant();
-    plant.id = id;
+  private calculateNextProcess(lastDate: Date, interval: ProcessInterval): Date {
+    const intervalsMap = new Map<ProcessInterval, { amount: number, unit: unitOfTime.Base }>([
+      [ProcessInterval.EVERYDAY, { amount: 1, unit: 'd' }],
+      [ProcessInterval.ONCE_A_WEEK, { amount: 1, unit: 'w' }],
+      [ProcessInterval.TWICE_A_WEEK, { amount: 3, unit: 'd' }],
+      [ProcessInterval.THREE_TIMES_A_WEEK, { amount: 2, unit: 'd' }],
+      [ProcessInterval.ONCE_A_MONTH, { amount: 1, unit: 'M' }],
+    ]);
+
+    const intervalMomentData = intervalsMap.get(interval);
+    return moment(lastDate).add(intervalMomentData.amount, intervalMomentData.unit).toDate();
+
+  }
+
+  async watering(id: number): Promise<any> {
+    const plant = await this.plantRepository.findOne(id);
+    if (!plant) throw new NotFoundException('Plant not found');
 
     const watering = new Watering();
     watering.date = new Date();
     watering.plant = plant;
 
-    return this.wateringRepository.save(watering);
+    plant.nextWatering = this.calculateNextProcess(watering.date, plant.wateringInterval);
+
+    // this should be transactional, update only after successful save
+    await this.wateringRepository.save(watering);
+    await this.plantRepository.update(id, plant);
+    return 'success';
   }
 
-  spraing(id: number): Promise<Spraing> {
-    const plant = new Plant();
-    plant.id = id;
+  async spraing(id: number): Promise<any> {
+    const plant = await this.plantRepository.findOne(id);
+    if (!plant) throw new NotFoundException('Plant not found');
 
     const spraing = new Spraing();
     spraing.date = new Date();
     spraing.plant = plant;
 
-    return this.spraingRepository.save(spraing);
+    plant.nextSpraing = this.calculateNextProcess(spraing.date, plant.spraingInterval);
+    // this should be transactional, update only after successful save
+    await this.spraingRepository.save(spraing);
+    await this.plantRepository.update(id, plant);
+    return 'success';
+
   }
 
-  feeding(id: number): Promise<Feeding> {
-    const plant = new Plant();
-    plant.id = id;
+  async feeding(id: number): Promise<any> {
+    const plant = await this.plantRepository.findOne(id);
+    if (!plant) throw new NotFoundException('Plant not found');
 
     const feeding = new Feeding();
     feeding.date = new Date();
     feeding.plant = plant;
 
-    return this.feedingRepository.save(feeding);
+    plant.nextFeeding = this.calculateNextProcess(feeding.date, plant.feedingInterval);
+    // this should be transactional, update only after successful save
+    await this.feedingRepository.save(feeding);
+    await this.plantRepository.update(id, plant);
+    return 'success';
+
   }
 
 
