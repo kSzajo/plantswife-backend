f0f31a8c3151f37a456aa35cf72153776964a20e
krzysztofszajowski <krzysztof.szajowski@gmail.com>
2020-09-20 22:27:00 +0200

    Add jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,20 @@ import { AuthController } from './controller/auth.controller';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './strategy/local.strategy';
 import { LocalAuthGuard } from './strategy/local-auth.guard';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { JwtAuthGuard } from './strategy/jwt-auth.guard';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService, LocalStrategy, LocalAuthGuard],
+  imports: [UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    })],
+  providers: [AuthService, LocalStrategy, LocalAuthGuard, JwtStrategy, JwtAuthGuard],
   controllers: [AuthController],
 })
 export class AuthModule {
--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from '../strategy/local-auth.guard';
+import { AuthService } from '../service/auth.service';
+import { JwtAuthGuard } from '../strategy/jwt-auth.guard';
 
 @Controller()
 export class AuthController {
 
+  constructor(private authService: AuthService) {
+  }
+
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return typeof req;
   }
 
 }
