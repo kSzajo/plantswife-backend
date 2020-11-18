c50f8ee093b9774d4de9a34ddd0304cfa9fe74c3
krzysztofszajowski <krzysztof.szajowski@gmail.com>
2020-11-18 15:31:19 +0100

    Add guard to process controller

--- src/plant/controller/plant-process.controller.ts
+++ src/plant/controller/plant-process.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Controller, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
 import { PlantProcessService } from '../service/plant-process.service';
 import { Watering } from '../entity/watering.entity';
 import { Spraing } from '../entity/spraing.entity';
 import { Feeding } from '../entity/feeding.entity';
+import { JwtAuthGuard } from '../../auth/strategy/jwt-auth.guard';
+import { IdentityGuard } from '../guard/identity.guard';
 
 @Controller('plants/:id')
 export class PlantProcessController {
@@ -10,16 +12,19 @@ export class PlantProcessController {
   }
 
   @Post('watering')
+  @UseGuards(JwtAuthGuard, IdentityGuard)
   watering(@Param('id', ParseIntPipe) id: number): Promise<Watering> {
     return this.plantService.watering(id);
   }
 
   @Post('spraing')
+  @UseGuards(JwtAuthGuard, IdentityGuard)
   spraing(@Param('id', ParseIntPipe) id: number): Promise<Spraing> {
     return this.plantService.spraing(id);
   }
 
   @Post('feeding')
+  @UseGuards(JwtAuthGuard, IdentityGuard)
   feeding(@Param('id', ParseIntPipe) id: number): Promise<Feeding> {
     return this.plantService.feeding(id);
   }
